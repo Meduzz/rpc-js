876da640f43271c475c3d335f2ec2d5665893384
Bjärn Bnegtsson <drgevalia@gmail.com>
2018-11-11 16:55:18 +0100

    Restructure a bit. Add 2 new methods to Message.

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-exports.nats = require('./nats')
-exports.model = require('./model.js')
-exports.local = require('./local')
+exports.nats = require('./lib/nats')
+exports.model = require('./lib/model.js')
+exports.local = require('./lib/local')
